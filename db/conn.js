8220f28ff4f94e2fa44a06d28995f5021d7143f7
DoubleCheese <zhuzhening1111@sina.com>
2022-12-13 19:54:46 +0000

    fix: db connection

--- db/conn.js
+++ db/conn.js
@@ -1,23 +1,18 @@
 const { MongoClient } = require("mongodb");
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
  
 var _db;
  
 module.exports = {
   connectToServer: function (callback) {
-    MongoClient.connect(process.env.ATLAS_URI,{ useUnifiedTopology: true, useNewUrlParser: true, })
-      .then((client,err)=>{
-      _db =client.db("vis30k");
-      console.log("Successfully connected to MongoDB."); 
-   })
-  //   client.connect(function (err, db) {
-  //     // Verify we got a good "db" object
-  //     if (db)
-  //     {
-  //       _db = db.db("vis30k");
-  //       console.log("Successfully connected to MongoDB."); 
-  //     }
-  //     return callback(err);
-  //        });
+    client.connect(err => {
+        _db = client.db("vis30k");
+        console.log("Successfully connected to MongoDB."); 
+      });
   },
  
   getDb: function () {
